fc6046a1bb872e7003aae19a9d94cb72f512f979
Abhijeet Kumar <82298229+Abhijeet0077@users.noreply.github.com>
2021-10-22 17:13:25 +0530

    Update settings.py

--- django/notejam/notejam/settings.py
+++ django/notejam/notejam/settings.py
@@ -14,12 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'notejam.db',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'USER': 'os.getenv('POSTGRES_USER')',
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
 
